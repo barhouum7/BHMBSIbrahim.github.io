72cdd95f52bf04d60542e31cfb3950b28a9286b7
barhouum7 <bb.med2000@gmail.com>
2020-05-27 22:25:34 +0100

    Added javascript file

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.btn-menu')
+const menu = document.querySelector('.menu')
+const menuNav = document.querySelector('.menu-nav')
+const menuBranding = document.querySelector('.menu-branding')
+const navItems = document.querySelectorAll('.nav-item')
+
+// Set Initial State Of the Menu
+
