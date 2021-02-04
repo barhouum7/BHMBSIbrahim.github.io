b5b16123ee040656365d3337b03a07c839008070
barhouum7 <bb.med2000@gmail.com>
2021-02-04 20:13:23 +0100

    Updated styles

--- dist/js/main.js
+++ dist/js/main.js
@@ -54,3 +54,54 @@ function changeActive(event) {
     // Add the active Class to the Current Clicked Element
     event.target.parentNode.classList.add('active')
 }
+
+
+const blocksContainer = document.querySelector('.projects')
+
+const projectBlocks = Array.from(blocksContainer.children)
+
+const projectBlocksOrderRange = [...Array(projectBlocks.length).keys()]
+
+// Add Order CSS Property To Project Blocks.
+
+    // Add A mouseover Event
+    function showModal(projectBlocks, message) {
+        projectBlocks.addEventListener('click', () => {
+            setTimeout(() => {
+                // Launching The Modal Box When focus on Cards 
+            document.querySelector('.modal-content p').innerHTML = message
+            // Creating the Modal's Variable
+            // Get the modal
+            var modal = document.getElementById("myModal");
+    
+            // Get the button that opens the modal
+            // var btn = document.getElementById("myBtn");
+    
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+    
+    
+            // When the user clicks on the button, open the modal
+            // btn.onclick = function() {
+                modal.style.display = "block";
+            // }
+    
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+    
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+            }, 500)
+        })
+    }
+
+    showModal(projectBlocks[0], '<div align="center"><h4>I developed the famous memory game. <br />You are given a set of cards with pictures that are flipped back, when you click on an image, it flips over and reveals the image. If you click on two exactly the same pics, that is a win-win for you.</h4> <h2>Technologies used 👨‍💻</h2> <h4>HTML, SASS and JavaScript.</h4></div>')
+    showModal(projectBlocks[1], '<div align="center"><h4>This a Neumorphic styled Tic Tac Toe Game using JavaScript. And I will make it better by turning it to a AI Game using the Minimax Algorithm. Neumorphism is a recent UI design trend. It’s also called “Soft UI”. It’s designs appear to extrude from the background.</h4><h2>Technologies used 👨‍💻</h2> <h4>HTML, SASS and JavaScript.</h4></div>')
+    showModal(projectBlocks[2], '<div align="center"><h4>This a Neumorphic styled Tic Tac Toe Game using JavaScript. And I will make it better by turning it to a AI Game using the Minimax Algorithm. Neumorphism is a recent UI design trend. It’s also called “Soft UI”. It’s designs appear to extrude from the background.</h4><h2>Technologies used 👨‍💻</h2> <h4>HTML, SASS and JavaScript.</h4></div>')
+    showModal(projectBlocks[3], '<div align="center"><h4>This a Neumorphic styled Tic Tac Toe Game using JavaScript. And I will make it better by turning it to a AI Game using the Minimax Algorithm. Neumorphism is a recent UI design trend. It’s also called “Soft UI”. It’s designs appear to extrude from the background.</h4><h2>Technologies used 👨‍💻</h2> <h4>HTML, SASS and JavaScript.</h4></div>')
